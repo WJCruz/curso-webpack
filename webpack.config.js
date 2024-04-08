26ab2377c66dc07e64d73c381fd02eaca7558040
Wilder Cruz <wilder.cruz@autofact.cl>
2024-04-07 20:04:58 -0500

    first commi

---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  // Entry nos permite decir el punto de entrada de nuestra aplicación
+  entry: './src/index.js',
+  // Output nos permite decir hacia dónde va enviar lo que va a preparar webpacks
+  output: {
+    // filename le pone el nombre al archivo final
+    filename: '[name].[contenthash].js',
+    // path es donde estará la carpeta donde se guardará los archivos
+    // Con path.resolve podemos decir dónde va estar la carpeta y la ubicación del mismo
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // Limpia la carpeta dist de tener archivos antiguos
+  },
+  resolve: {
+    // Aqui ponemos las extensiones que tendremos en nuestro proyecto para webpack los lea
+    extensions: ['.js'],
+    alias: {
+      '@utils': path.resolve(__dirname, 'src/utils/'),
+      '@templates': path.resolve(__dirname, 'src/templates/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@images': path.resolve(__dirname, 'src/assets/images/'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        // Test declara que extensión de archivos aplicara el loader
+        test: /\.m?js$/,
+        // Use es un arreglo u objeto donde dices que loader aplicaras
+        use: {
+          loader: "babel-loader",
+        },
+        // Exclude permite omitir archivos o carpetas especificas
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(css|styl)$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "stylus-loader",
+        ]
+      },
+      {
+        test: /\.png/,
+        type: "asset/resource",
+        generator: {
+          filename: 'assets/images/[name].[contenthash][ext][query]',
+        },
+      },
+      {
+        test: /\.(woff|woff2)$/i,  // Tipos de fuentes a incluir
+        type: 'asset/resource',  // Tipo de módulo a usar (este mismo puede ser usado para archivos de imágenes)
+        generator: {
+          filename: 'assets/fonts/[hash][ext][query]',  // Directorio de salida
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        inject: true, // Esta opción permite la inserción automática de etiquetas css y js
+        template: './public/index.html', // ruta donde se encuentra el archivo html
+        filename: './index.html' // nombre y ubicación de salida
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].[contenthash].css',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images",
+        },
+      ],
+    }),
+    new Dotenv(), // permite leer las variables de entorno
+    // new CleanWebpackPlugin(), // Limpia la carpeta dist de tener archivos antiguos
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin(),
+    ],
+  },
+};
